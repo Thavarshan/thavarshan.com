3b2f4bd6f4f46e082607bcd042c19e509d9fb022
thavarshan <tjthavarshan@gmail.com>
2020-07-17 19:29:21 +0530

    Add website links to github repo

--- src/components/GitHub.vue
+++ src/components/GitHub.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
         <div class="flex flex-col h-48" v-for="repo in repos" :key="repo.id">
-            <a target="_blank" :href="repo.html_url" class="bg-white rounded-lg shadow-sm hover:shadow-md p-4 flex flex-col justify-between leading-normal flex flex-col flex-1">
+            <a target="_blank" :href="repo.homepage ? repo.homepage : repo.html_url" class="bg-white rounded-lg shadow-sm hover:shadow-md p-4 flex flex-col justify-between leading-normal flex flex-col flex-1">
                 <Card :repo="repo" />
 
                 <div class="flex items-baseline justify-between">
