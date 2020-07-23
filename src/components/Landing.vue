ec90bd9615c614af4c42c6e844a3c9301fdd18a4
thavarshan <tjthavarshan@gmail.com>
2020-07-24 01:36:32 +0530

    Disable recommendations section

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -7,6 +7,8 @@
         <main role="main">
             <Profile />
 
+            <!-- <Recommendations /> -->
+
             <Projects />
         </main>
 
@@ -21,7 +23,7 @@
     import Profile from './Profile';
     import Projects from './Projects';
     import Credits from './Credits';
-    import Recommendations from './Recommendations';
+    // import Recommendations from './Recommendations';
 
     export default {
         components: {
@@ -29,7 +31,7 @@
             Profile,
             Projects,
             Credits,
-            Recommendations
+            // Recommendations
         },
     }
 </script>
