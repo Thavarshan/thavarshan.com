5d22723337bfa7ef2c9d22c439c5607b56958259
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-03 03:38:34 +0530

    Add unit test modules

--- pages/sections/Profile.vue
+++ pages/sections/Profile.vue
@@ -47,7 +47,5 @@
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
---
+++ test/Card.spec.js
@@ -0,0 +1,10 @@
+import { mount } from '@vue/test-utils';
+import Card from '@/components/Card.vue';
+
+describe('Card', () => {
+    test('is a Vue instance', () => {
+        const wrapper = mount(Card);
+
+        expect(wrapper.vm).toBeTruthy();
+    });
+});
