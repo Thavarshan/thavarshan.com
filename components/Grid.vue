8a06c221a0f1945fb28448e8dd37a109832f96ee
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2021-06-09 07:30:30 +0530

    Add project constraint method to grid component

--- components/Grid.vue
+++ components/Grid.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="space-y-6 lg:space-y-0 lg:grid lg:grid-cols-12 gap-6">
-        <div v-for="(project, id) in projects" :key="id" class="col-span-full sm:col-span-6 md:col-span-4 lg:col-span-3 flex flex-col">
+        <div v-for="(project, id) in projects" :key="id" v-show="isShowable(project)" class="col-span-full sm:col-span-6 md:col-span-4 lg:col-span-3 flex flex-col">
             <card :project="project"></card>
         </div>
     </div>
@@ -8,6 +8,12 @@
 
 <script>
 export default {
-    props: ['projects']
+    props: ['projects'],
+
+    methods: {
+        isShowable(project) {
+            return ! project.fork && project.name !== 'automation';
+        }
+    }
 }
 </script>
