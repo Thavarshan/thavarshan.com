512bb199a9070454666885969bcac7bef4d4df1b
Thavarshan Thayananthajothy <tjthavarshan@gmail.com>
2022-09-19 11:15:43 +0530

    Update profile description

--- pages/sections/Profile.vue
+++ pages/sections/Profile.vue
@@ -2,39 +2,29 @@
     <div class="py-16">
         <div class="container mx-auto px-4 sm:px-6">
             <div class="lg:grid lg:grid-cols-12 gap-6">
-                <div class="col-start-3 col-end-11 col-span-7">
-                    <div class="md:flex items-start">
+                <div class="col-start-4 col-end-10 col-span-5">
+                    <div class="md:flex items-start justify-center">
                         <div class="h-32 w-32 mx-auto md:m-0 rounded-full overflow-hidden">
                             <img class="h-32 w-32" src="https://www.gravatar.com/avatar/bbaa687375c07191a976b8a98205e178?s=200" alt="Thavarshan">
                         </div>
 
-                        <div class="mt-10 md:mt-0 md:ml-10 flex-1 text-center md:text-left">
+                        <div class="mt-10 md:mt-0 md:ml-10 flex-1 items-center md:text-left">
                             <div>
                                 <h4 class="font-bold text-gray-800 text-2xl">
                                     So, who am I?
                                 </h4>
 
                                 <p class="mt-4 leading-relaxed text-lg">
-                                    I'm <a class="text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700" href="mailto:tjthavarshan@gmail.com">@thavarshan</a>, a professional Software Engineer, Graphic design enthusiate, and part-time Artist.
+                                    I'm <a class="text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700" href="mailto:tjthavarshan@gmail.com">@thavarshan</a>, a professional Software Engineer, Graphic design enthusiate, and part-time Artist. I love to create, make, and build stuff. I spend countless hours sitting in front of a computer screen making something amazing and when I'm not, I draw pencil sketch portraits.
                                 </p>
 
                                 <p class="mt-4 leading-relaxed text-lg">
-                                    Essentially, I love to create, make, and build stuff. I spend countless hours sitting in front of a computer screen making something amazing and when I'm not, I draw pencil sketch portraits.
+                                    I used to love PHP and had been using it for at least seven years and more but I have now switched to Typescript. So, currently I'm trying to expand my knowledge about the other tools in my arsenal and at the same time, try to complete as many personal and professional projects as I can. Take a look down below to see what I'm up to on <a class="text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700" target="_blank" href="https://github.com/Thavarshan">GitHub</a> and <a class="text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700" target="_blank" href="https://bitbucket.org/thavarshan/">BitBucket</a>.
                                 </p>
 
-                                <p class="mt-4 leading-relaxed text-lg">
-                                    I pride myself in knowing six programming languages, C, C++, PHP, Python, JavaScript, and Java of which PHP is my main game. I love PHP and have been using it for at least six years. So, currently I'm trying to expand my knowledge about the other tools in my arsenal and at the same time, try to complete as many personal and professional projects as I can. Take a look down below to see what I'm up to on <a class="text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700" target="_blank" href="https://github.com/Thavarshan">GitHub</a> and <a class="text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700" target="_blank" href="https://bitbucket.org/thavarshan/">BitBucket</a>.
-                                </p>
-
-                                <div class="mt-6">
-                                    <a class="inline-flex items-center text-blue-500 hover:text-blue-600 focus:text-blue-600 active:text-blue-700 font-semibold" href="mailto:tjthavarshan@gmail.com">
+                                <div class="mt-6 flex justify-start space-x-6 text-sm">
+                                    <a class="bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 focus:ring-offset-2 focus:ring-offset-blue-50 text-white font-semibold h-12 px-6 rounded-full w-full flex items-center justify-center sm:w-auto dark:bg-blue-500 dark:highlight-white/20 dark:hover:bg-blue-400" href="mailto:tjthavarshan@gmail.com">
                                         <span>Contact me</span>
-
-                                        <span class="ml-1">
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-                                                <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                            </svg>
-                                        </span>
                                     </a>
                                 </div>
                             </div>
