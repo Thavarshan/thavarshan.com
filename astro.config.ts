7f23b85e3198cbc5759b7f7a3ca4557f949e6ccc
Jerome (Thavarshan) Thayananthajothy <jerome@netwatchglobal.com>
2024-04-25 23:52:59 +0100

    Add astro-icon integration and add animated background

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
 import markdown from "./integrations/markdown";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), markdown()],
+    integrations: [tailwind(), markdown(), icon()],
     site: "https://thavarshan.com"
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,21 @@ module.exports = {
 		extend: {
 			colors: {
 				main: "#78350f",
+                animated: "#fffb76",
+                amber: {
+                    DEFAULT: '#FEAD38',
+                    50: '#FFF8EF',
+                    100: '#FFF0DA',
+                    200: '#FFDFB2',
+                    300: '#FECE89',
+                    400: '#FEBE61',
+                    500: '#FEAD38',
+                    600: '#FD9601',
+                    700: '#C57501',
+                    800: '#8D5401',
+                    900: '#553300',
+                    950: '#392200'
+                },
 			},
 			fontSize: {
 				"code-sm": "0.825rem",
