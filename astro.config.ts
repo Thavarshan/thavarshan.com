980c2575b4c3d235b941aacf489421d250ff30df
Jerome (Thavarshan) Thayananthajothy <jerome@netwatchglobal.com>
2024-04-27 23:54:55 +0100

    Add vercel analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from "astro/config";
-import markdown from "./integrations/markdown";
-import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
+import { defineConfig } from 'astro/config';
+import markdown from './integrations/markdown';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), markdown(), icon()],
-    site: "https://thavarshan.com"
+    site: 'https://thavarshan.com',
+    output: 'server',
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    }),
 });
