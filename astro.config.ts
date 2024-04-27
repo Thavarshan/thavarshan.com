1ea15afbd2f55e2b3a293df7d6fa5d2934794a7b
Jerome (Thavarshan) Thayananthajothy <jerome@netwatchglobal.com>
2024-04-28 00:00:58 +0100

    Upgrade dependencies and fix vercel analytics issue

--- astro.config.ts
+++ astro.config.ts
@@ -2,14 +2,17 @@ import { defineConfig } from 'astro/config';
 import markdown from './integrations/markdown';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), markdown(), icon()],
     site: 'https://thavarshan.com',
-    output: 'server',
+    output: "server",
     adapter: vercel({
-        webAnalytics: { enabled: true }
-    }),
+        webAnalytics: {
+            enabled: true,
+        },
+    })
 });
